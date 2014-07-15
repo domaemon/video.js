5b98093b8479ade9f7ab9fff564e1c3d18d453db
Makoto Sugano <makoto.sugano@gmail.com>
2014-07-15 12:16:42 +0900

    exporting the textTrack functions.

---
+++ src/js/tracks.externs.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Externs for videojs.TextTrack. Externs are functions that the
+ * compiler shouldn't obfuscate.
+ */
+
+/**
+ * @constructor
+ * @extends {videojs.Component}
+ */
+videojs.TextTrack = function(){};
+
+/**
+ * Text tracks
+ */
+videojs.TextTrack.prototype.parseCues = function(){};
